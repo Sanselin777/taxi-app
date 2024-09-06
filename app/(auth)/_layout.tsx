e37698b01214ca3a37e0e397b09b055849b0ec7b
oscar-zarzilla <oscar-zarzilla@io2mail.com>
2024-09-06 18:04:51 -0500

    images

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <Stack>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
-      <Stack.Screen name="sing-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
       <Stack.Screen name="sign-in" options={{ headerShown: false }} />
     </Stack>
   );
--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,29 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { data } from "@/constants";
 
 const Welcome = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Text>Welcome</Text>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper>
+        {data.onboarding.map((item, index) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
